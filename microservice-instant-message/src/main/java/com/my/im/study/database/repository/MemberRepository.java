6ec6ec4910d2f14b799d56b99c4affdb01b66ae4
tairitsu86 <benimiku9@gmail.com>
2023-03-21 12:05:00 +0800

    update .gitignore

--- microservice-instant-message/src/main/java/com/my/im/study/database/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.my.im.study.database.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.my.im.study.database.entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member, String>{
-}
--- microservice-instant-message/src/main/java/com/my/im/study/database/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.my.im.study.database.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.my.im.study.database.entity.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
